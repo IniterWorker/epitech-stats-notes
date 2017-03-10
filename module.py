1b7312cd8d7b0f961f1eddbbb21ee66db8f17701
Walter Bonetti <walter.bonetti@epitech.eu>
2017-03-10 12:34:14 +0100

    modul API urlFormater

--- module.py
+++ module.py
@@ -1,26 +1,17 @@
 import requests
-from config import Configure
+from api import IntranetAPI
 
 class ModuleNotes:
     """
     This class can take all notes of the specified module Code
     """
 
-    host = "https://intra.epitech.eu/"
-    user_api = "/user/"
-    notes_api = "notes/?format=json"
-
-    def __init__(self, config, moduleCode, login):
-        if not isinstance(config, Configure):
-            raise TypeError("Force Configure instance")
-        if config.getAuth() == "None":
-            raise ValueError("Please check auth key")
-        self.req = requests.get(self.urlAuthEncode(login, config.getAuth()) + ModuleNotes.notes_api)
+    def __init__(self, Api, moduleCode, login):
+        if not isinstance(Api, IntranetAPI):
+            raise TypeError("Force Intranet instance")
+        self.req = requests.get(Api.urlFormatedWithUser("notes", login))
         # search only code module
         self.note = [x for x in self.req.json()["notes"] if "codemodule" in x and x["codemodule"] == moduleCode]
-
-    def urlAuthEncode(self, login, auth):
-        return ModuleNotes.host + auth + ModuleNotes.user_api + login + "/"
 
     def getNotes(self):
         return self.note
